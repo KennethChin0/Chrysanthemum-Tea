c69f3bb3c9d991d2693e0c582ba888c3468763b0
jchen9182 <jchen9182@gmail.com>
2020-06-10 16:40:51 -0400

    page resizing sorta works

--- app/static/homepage.js
+++ app/static/homepage.js
@@ -1,6 +1,6 @@
 // GLOBAL VARIABLES
 var running = false; // Don't let a user spam the button
-
+var currentID = "";
 
 // BUTTON GROUP
 var chooseGraph = function(e) {
@@ -14,7 +14,9 @@ var chooseGraph = function(e) {
         e.target.className = "btn btn-primary";
 
         running = true;
-        initGraph(e.target.id);
+        currentID = e.target.id;
+        draw();
+        running = false;
     }
 }
 
@@ -28,18 +30,35 @@ var button3 = document.getElementById("b3");
 button3.addEventListener("click", chooseGraph);
 
 
+// WINDOW RESIZE
+var hConst, wConst;
+
+function draw() {
+    hConst = window.innerHeight / screen.availHeight;
+    hConst = hConst < 1 ? hConst + .068 : 1 // Make up for rough calculation
+    wConst = window.innerWidth / screen.availWidth;
+
+    if (currentID != "") {
+        initGraph(currentID);
+        initPie(currentID);
+    }
+}
+window.addEventListener("resize", draw);
+
+
 // FETCH AND FORMAT DATA
-var jsonData = document.getElementById("data").value;
-data = JSON.parse(jsonData);
 var parseTime = d3.timeParse("%Y-%m-%d");
 var formatTime = d3.timeFormat("%Y-%m-%d");
+
+var jsonData = document.getElementById("data").value;
+data = JSON.parse(jsonData);
 
 // Sort dates in ascending order
 data.sort(function(a, b) {
     return parseTime(a[1]) - parseTime(b[1]);
 });
 
-// console.log(data);
+console.log(data);
 
 
 // GRAPHS
@@ -54,131 +73,94 @@ function initGraph(id) {
     var range;
 
     // Draw Graph
-    if (id == "b1") {
-        var timeNow = new Date();
-        timeNow.setHours(0, 0, 0);
-        timeNow = formatTime(timeNow);
-
-        var date = timeNow.split("-");
-        var year = date[0];
-        var month = date[1];
-        var day = date[2];
-
-        if (month == 1) {
-            date[0] = String(year - 1);
-            date[1] = "12";
-        }
-        else {
-            if (month == 3 && day > 28) {
-                date[2] = "28";
-                if (year % 4 == 0 && year % 100 != 0) date[2] = "29";
-                else if (year % 400 == 0) date[2] = "29";
-                
-            }
-            else if (month != 8 && day == 31) {
-                date[2] = "30";
-            }
-            date[1] = "0" + String(month - 1);
-        }
-        var firstDate = date.join("-");
-        firstDate = parseTime(firstDate);
-
-        for (var i = 0; i < data.length; i++) {
-            var dataDate = parseTime(data[i][1]);
-            if (firstDate <= dataDate) {
-                cash += data[i][2];
-                money.push(cash);
-
-                dates.push(dataDate);
-            }
-        }
-
-        if (dates.length == 0 || money.length == 0) {
-            document.getElementById("graph").innerHTML = 
-            "<br><br> Uhoh. There's nothing here. &#128557";
-        }
-        else {
-            createGraph(dates, money, 0);
-        }
-    }
-
-    else if (id == "b2") {
-        var timeNow = new Date();
-        timeNow.setHours(0, 0, 0);
-        timeNow = formatTime(timeNow);
-
-        var date = timeNow.split("-");
-        var year = date[0];
-        var month = date[1];
-        var day = date[2];
-
-        if (month > 6) {
-            if (month == 8 && day > 28) {
-                date[2] = "28";
-                if (year % 4 == 0 && year % 100 != 0) date[2] = "29";
-                else if (year % 400 == 0) date[2] = "29";
-                
-            }
-            else if (day == 31) {
-                date[2] = "30"
-            }
-            date[1] = "0" + String(month - 6);
-        }
-        else {
-            if (day == 31) {
-                date[2] = "30";
-            }
-            date[0] = String(year - 1);
-            date[1] = "0" + String(month + 6);
-        }
-
-        var firstDate = date.join("-");
-        firstDate = parseTime(firstDate);
-
-        for (var i = 0; i < data.length; i++) {
-            var dataDate = parseTime(data[i][1]);
-            if (firstDate <= dataDate) {
-                cash += data[i][2];
-                money.push(cash);
-
-                dates.push(dataDate);
-            }
-        }
-
-        if (dates.length == 0 || money.length == 0) {
-            document.getElementById("graph").innerHTML = 
-            "<br><br> Uhoh. There's nothing here. &#128557";
-        }
-        else {
-            createGraph(dates, money, 0);
-        }
-    }
-
-    else if (id == "b3") {
+    if (id == "b3") {
         for (var i = 0; i < data.length; i++) {
             cash += data[i][2];
             money.push(cash);
 
             dates.push(parseTime(data[i][1]));
         }
-        
-        if (dates.length == 0 || money.length == 0) {
-            document.getElementById("graph").innerHTML = 
-            "<br><br> Uhoh. There's nothing here. &#128557";
+    }
+    else {
+        var timeNow = new Date();
+        timeNow.setHours(0, 0, 0);
+        timeNow = formatTime(timeNow);
+
+        var date = timeNow.split("-");
+        var year = date[0];
+        var month = date[1];
+        var day = date[2];
+
+        if (id == "b1") {
+            if (month == 1) {
+                date[0] = String(year - 1);
+                date[1] = "12";
+            }
+            else {
+                if (month == 3 && day > 28) {
+                    date[2] = "28";
+                    if (year % 4 == 0 && year % 100 != 0) date[2] = "29";
+                    else if (year % 400 == 0) date[2] = "29";
+                    
+                }
+                else if (month != 8 && day == 31) {
+                    date[2] = "30";
+                }
+                date[1] = "0" + String(month - 1);
+            }
         }
-        else {
-            createGraph(dates, money, 0);
+    
+        else if (id == "b2") {
+            if (month > 6) {
+                if (month == 8 && day > 28) {
+                    date[2] = "28";
+                    if (year % 4 == 0 && year % 100 != 0) date[2] = "29";
+                    else if (year % 400 == 0) date[2] = "29";
+                    
+                }
+                else if (day == 31) {
+                    date[2] = "30"
+                }
+                date[1] = "0" + String(month - 6);
+            }
+            else {
+                if (day == 31) {
+                    date[2] = "30";
+                }
+                date[0] = String(year - 1);
+                date[1] = "0" + String(month + 6);
+            }
+        }
+
+        var firstDate = date.join("-");
+        firstDate = parseTime(firstDate);
+
+        for (var i = 0; i < data.length; i++) {
+            var dataDate = parseTime(data[i][1]);
+            if (firstDate <= dataDate) {
+                cash += data[i][2];
+                money.push(cash);
+
+                dates.push(dataDate);
+            }
         }
     }
 
-    running = false;
+    if (dates.length == 0 || money.length == 0) {
+        document.getElementById("nothing").innerHTML = 
+        "<br><br> Uhoh. There's nothing here. &#128557";
+    }
+    else {
+        createGraph(dates, money, 0);
+    }
 }
 
 function createGraph(dates, money, range) {
-    var svgHeight = 600;
-    var svgWidth = 600;
+    var svgHeight = 600 * hConst;
+    var svgWidth = 750 * wConst;
 
-    var margins = { top: 50, bottom: 50, left: 50, right: 50 };
+    var margins = { top: 50 * hConst, bottom: 50 * hConst,
+                    left: 50 * wConst, right: 50 * wConst};
     var height = svgHeight - margins.top - margins.bottom;
     var width = svgWidth - margins.left - margins.right;
 
@@ -226,4 +208,12 @@ function createGraph(dates, money, range) {
             .y(d => yAxis(d.cash)));
     
     // Add labels
+}
+
+function initPie(id) {
+    return;
+}
+
+function createPie() {
+    return;
 }
