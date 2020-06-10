f39ca8d378c2481e2cb96cb52127702ad4a782be
jchen9182 <jchen9182@gmail.com>
2020-06-09 22:10:26 -0400

    fixes

--- app/static/homepage.js
+++ app/static/homepage.js
@@ -1,17 +1,41 @@
-var changeButtonState = function(e) {
-    // Reset clicked button to original state
-    var button = document.getElementsByClassName("btn btn-primary");
-    if (button.length > 0) button[0].className = "btn btn-outline-primary";
+// Global variables
+var running = false; // Don't let a user spam the button
 
-    // Change this button's state
-    e.path[0].className = "btn btn-primary";
+// BUTTON GROUP
+var chooseGraph = function(e) {
+    if (!running) {
+        // Reset clicked button to original state
+        var button = document.getElementsByClassName("btn btn-primary");
+        if (button.length > 0)
+            button[0].className = "btn btn-outline-primary";
+
+        // Change this button's state (fills button in)
+        e.path[0].className = "btn btn-primary";
+
+        running = true;
+        initGraph(e.path[0].id);
+    }
 }
 
 var button1 = document.getElementById("b1");
-button1.addEventListener("click", changeButtonState);
+button1.addEventListener("click", chooseGraph);
 
 var button2 = document.getElementById("b2");
-button2.addEventListener("click", changeButtonState);
+button2.addEventListener("click", chooseGraph);
 
 var button3 = document.getElementById("b3");
-button3.addEventListener("click", changeButtonState);
+button3.addEventListener("click", chooseGraph);
+
+// GRAPHS
+function initGraph(id) {
+    if (id == "b1") {
+        console.log(1);
+    }
+    if (id == "b2") {
+        console.log(2);
+    }
+    if (id == "b3") {
+        console.log(3);
+    }
+    running = false;
+}
